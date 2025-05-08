0d1357939a2b818b6d2614aa6302f3ee1631f03d
leilei-anthony <lesteranthonyjr@gmail.com>
2025-05-08 20:02:13 +0800

    Learned Webpacks

--- factory-functions-and-the-module-pattern/script.js
+++ factory-functions-and-the-module-pattern/script.js
@@ -95,4 +95,19 @@ function createUser (name) {
     }
   }
   
+
+  function SuperElement(type, content) {
+    this.el = document.createElement(type);
+    this.el.innerText = content;
+    document.body.append(this.el);
+    this.el.addEventListener('click', () => {
+      console.log(this.el);
+    })
+  }
+
+  const array = ['a', 'b', 'c'];
+
+  const myElements = array.map(item => {
+    return new SuperElement('p', item);
+  })
   
